e24981bce50c4322ceba3240e7784114881878c0
Porscha07 <porscha.uga@gmail.com>
2017-07-21 17:37:46 -0400

    added compass files for write page and added write component/route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import Register from './containers/Register';
 import Home from './components/Home'; 
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import NavBar from './components/NavBar';
+import Write from './components/Write';
 // import Home from './components/Home';
 
 // import logo from './logo.svg';
@@ -18,6 +19,8 @@ class App extends Component {
             <NavBar/>
             <Route exact path="/" component={Home} />
             <Route exact path="/signup" component={Register} />
+            <Route exact path="/write" component={Write} />
+
           </div>
         </Router>
       </div> 
