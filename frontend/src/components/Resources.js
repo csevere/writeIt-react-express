234e05a5dc28924fdceeb05e3ac75fc1d7c4af15
Porscha07 <porscha.uga@gmail.com>
2017-07-28 11:40:12 -0400

    added table for resources component, created sass file for component as well.

--- frontend/src/components/Resources.js
+++ frontend/src/components/Resources.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import { Link} from 'react-router-dom';
-import { Grid, Row, Col } from 'react-bootstrap';
+import { Grid, Row, Col,Table,Button } from 'react-bootstrap';
 import ScrollableAnchor from 'react-scrollable-anchor';
 
 class Resources extends Component{
@@ -11,6 +11,33 @@ class Resources extends Component{
 					<Row>
 						<Col lg={12} >
 								<h1>Resources Page</h1>
+								<Table class="resources-table" striped bordered responsive>
+								    <thead>
+								      <tr>
+								        <th>Book</th>
+								        <th>Title & Author</th>
+								        <th>Description</th>
+								      </tr>
+								    </thead>
+								    <tbody>
+								      <tr>
+								        <td></td>
+								        <td></td>
+								        <td></td>
+
+								      </tr>
+								      <tr>
+								        <td></td>
+								        <td></td>
+								        <td></td>
+								      </tr>
+								      <tr>
+								        <td></td>
+								        <td></td>
+								        <td></td>
+								      </tr>
+								    </tbody>
+								</Table>
 						</Col>
 					</Row>
 				</Grid>
