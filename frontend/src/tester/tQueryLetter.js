285df1512ba75d604238d51ed6c2b8bfe0fa324c
Carlita <carla.severe@gmail.com>
2017-07-28 02:58:07 -0400

    fixed some merge conflicts

--- frontend/src/tester/tQueryLetter.js
+++ frontend/src/tester/tQueryLetter.js
@@ -9,6 +9,9 @@ import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import QueryLetterAction from '../actions/QueryLetterAction';
+
+
+
 class tQueryLetter extends Component{
   constructor(props) {
     super(props);
@@ -21,6 +24,7 @@ class tQueryLetter extends Component{
     this.handleQueryLetter = this.handleQueryLetter.bind(this);
     
   }
+
   handleQueryLetter(event){
     event.preventDefault();
     
@@ -75,35 +79,7 @@ class tQueryLetter extends Component{
       });
     }
   }
-<<<<<<< HEAD
-=======
 
-
-
-class tQueryLetter extends Component{
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     registerMessage: "",
-  //     nameError: null,
-  //     emailError: null,
-  //     formError: false
-  //   }
-  //   this.handlePlot = this.handlePlot.bind(this);
-    
-  // }
-
-  // handlePlot(event){
-  //   event.preventDefault();
-    
-  //   var error = false;
-
-  //   var name = event.target[0].value;
-  //   var race = event.target[1].value;
-  //   var age = event.target[2].value;
-  //   var birthday = event.target[3].value;
-  //   // var physical_desc = event.target[4].value;
->>>>>>> Porscha'sBranch
  
     render(){
       const settings = {
@@ -116,93 +92,96 @@ class tQueryLetter extends Component{
     console.log(this.props)
         return(
             <div>
-                <Grid className = "writemenucat text-center ch-forms">
-                    <Row>
-              <Col sm = {8} md = {10} className = "ch-forms-2 col-md-offset-3">
-                <Slider {...settings}>
-                  <div className = "slick-form 1">
-                    <Form onSubmit={this.handleQueryLetter}>
-                        <FormGroup controlId="formControlsTextarea">
-                            <ControlLabel>Have you researched the agent and the type of authods he/she represents?</ControlLabel>
-                            <FormControl id="query_clarity" componentClass="textarea" placeholder="query_clarity" />
-                        </FormGroup>
-                        <FormGroup controlId="formControlsTextarea">
-                            <ControlLabel>In setting up the story, specify exactly what your character wants the most in the world?</ControlLabel>
-                            <FormControl id="query_boring" componentClass="textarea" placeholder="query_boring" />
-                        </FormGroup>
-                        <FormGroup controlId="formControlsTextarea">
-                            <ControlLabel>Specify what's getting in the way of your character's goals?</ControlLabel>
-                            <FormControl id="query_balance" componentClass="textarea" placeholder="query_balance" />
-                        </FormGroup>
-                        <FormGroup controlId="formControlsTextarea">
-                            <ControlLabel>Whats at stake if your character fails?</ControlLabel>
-                            <FormControl id="query_stakes" componentClass="textarea" placeholder="query_stakes" />
-                        </FormGroup>
-                        <FormGroup controlId="formControlsTextarea">
-                            <ControlLabel>How can you show rather than tell the agent about your main character?</ControlLabel>
-                            <FormControl id="query_advance" componentClass="textarea" placeholder="query_advance" />
-                        </FormGroup>
-                        <Button className = "btn" type="submit">
-                            submit
-                        </Button>                 
-                      </Form>
-
-                    </div> 
-                    <div className = "slick-form 2">
-                      <Form onSubmit={this.handleQueryLetter}>
-                        <FormGroup controlId="formControlsTextarea">
-                            <ControlLabel>How can you show your characters emotional journey from start to end?</ControlLabel>
-                              <FormControl id="query_resolve" componentClass="textarea" placeholder="query_resolve" />
-                          </FormGroup>
-                          <FormGroup controlId="formControlsTextarea">
-                            <ControlLabel>What are the stats of your book: Genre,target audience, word count?</ControlLabel>
-                              <FormControl id="query_voice" componentClass="textarea" placeholder="query_voice" />
-                          </FormGroup>
-                          <FormGroup controlId="formControlsTextarea">
-                            <ControlLabel>What comtemporary titles match your book?</ControlLabel>
-                              <FormControl id="query_action" componentClass="textarea" placeholder="query_action" />
-                          </FormGroup>
-                          <FormGroup controlId= "formControlsTextarea">
-                            <ControlLabel>Is your bio too long?</ControlLabel>
-                              <FormControl id="query_personality" componentClass="textarea" placeholder="query_personality" />
-                          </FormGroup>
-                          <FormGroup controlId="formControlsTextarea">
-                            <ControlLabel>Which sentences generate multiple questions? Rewrite them here.</ControlLabel>
-                              <FormControl id="query_romance" componentClass="textarea" placeholder= "query_romance" />
-                          </FormGroup>
-                          <Button className = "btn" type="submit">
-                              submit
-                          </Button>             
-                        </Form>
-                      </div> 
-                  </Slider> 
-                </Col>
+              <Grid className = "writemenucat text-center ch-forms">
                 <Row>
-                  <Grid className = "fourth-row-left">
-                    <Col md = {3}>
-                      <Link to = "/writeMenu" className = "return-writemenu">
-                        <img src = "https://cdn4.iconfinder.com/data/icons/lifestyle-set-2/100/07a3c3443f894cb3fa7a93ee3c496233-512.png"/>
-                        <div>Return to Write Menu</div>
-                      </Link>
-                    </Col> 
-                  </Grid>
+                  <Col sm = {8} md = {10} className = "ch-forms-2 col-md-offset-3">
+                    <Slider {...settings}>
+                      <div className = "slick-form 1">
+                        <Form onSubmit={this.handleQueryLetter}>
+                            <FormGroup controlId="formControlsTextarea">
+                                <ControlLabel>Have you researched the agent and the type of authods he/she represents?</ControlLabel>
+                                <FormControl id="query_clarity" componentClass="textarea" placeholder="query_clarity" />
+                            </FormGroup>
+                            <FormGroup controlId="formControlsTextarea">
+                                <ControlLabel>In setting up the story, specify exactly what your character wants the most in the world?</ControlLabel>
+                                <FormControl id="query_boring" componentClass="textarea" placeholder="query_boring" />
+                            </FormGroup>
+                            <FormGroup controlId="formControlsTextarea">
+                                <ControlLabel>Specify what's getting in the way of your character's goals?</ControlLabel>
+                                <FormControl id="query_balance" componentClass="textarea" placeholder="query_balance" />
+                            </FormGroup>
+                            <FormGroup controlId="formControlsTextarea">
+                                <ControlLabel>Whats at stake if your character fails?</ControlLabel>
+                                <FormControl id="query_stakes" componentClass="textarea" placeholder="query_stakes" />
+                            </FormGroup>
+                            <FormGroup controlId="formControlsTextarea">
+                                <ControlLabel>How can you show rather than tell the agent about your main character?</ControlLabel>
+                                <FormControl id="query_advance" componentClass="textarea" placeholder="query_advance" />
+                            </FormGroup>
+                            <Button className = "btn" type="submit">
+                                submit
+                            </Button>                 
+                          </Form>
 
-                  <Grid className = "fourth-row-right">
-                    <Col md = {3} className = "col-md-offset-8">
-                      <Link to = "/qlboard" className = "qlboard">
-                        <img src = "https://cdn4.iconfinder.com/data/icons/office-34/256/10-512.png"/>
-                        <div>View Query Letter</div>
-                      </Link>
-                    </Col>
-                  </Grid>
-                </Row> 
-                   
-            </Row>    
-              </Grid>   
+                        </div> 
+                        <div className = "slick-form 2">
+                          <Form onSubmit={this.handleQueryLetter}>
+                            <FormGroup controlId="formControlsTextarea">
+                                <ControlLabel>How can you show your characters emotional journey from start to end?</ControlLabel>
+                                  <FormControl id="query_resolve" componentClass="textarea" placeholder="query_resolve" />
+                              </FormGroup>
+                              <FormGroup controlId="formControlsTextarea">
+                                <ControlLabel>What are the stats of your book: Genre,target audience, word count?</ControlLabel>
+                                  <FormControl id="query_voice" componentClass="textarea" placeholder="query_voice" />
+                              </FormGroup>
+                              <FormGroup controlId="formControlsTextarea">
+                                <ControlLabel>What comtemporary titles match your book?</ControlLabel>
+                                  <FormControl id="query_action" componentClass="textarea" placeholder="query_action" />
+                              </FormGroup>
+                              <FormGroup controlId= "formControlsTextarea">
+                                <ControlLabel>Is your bio too long?</ControlLabel>
+                                  <FormControl id="query_personality" componentClass="textarea" placeholder="query_personality" />
+                              </FormGroup>
+                              <FormGroup controlId="formControlsTextarea">
+                                <ControlLabel>Which sentences generate multiple questions? Rewrite them here.</ControlLabel>
+                                  <FormControl id="query_romance" componentClass="textarea" placeholder= "query_romance" />
+                              </FormGroup>
+                              <Button className = "btn" type="submit">
+                                  submit
+                              </Button>             
+                            </Form>
+                          </div> 
+                      </Slider> 
+                      </Col>
+                      <Row>
+                        <Grid className = "fourth-row-left">
+                          <Col md = {3}>
+                            <Link to = "/writeMenu" className = "return-writemenu">
+                              <img src = "https://cdn4.iconfinder.com/data/icons/lifestyle-set-2/100/07a3c3443f894cb3fa7a93ee3c496233-512.png"/>
+                              <div>Return to Write Menu</div>
+                            </Link>
+                          </Col> 
+                        </Grid>
+
+                        <Grid className = "fourth-row-right">
+                          <Col md = {3} className = "col-md-offset-8">
+                            <Link to = "/qlboard" className = "qlboard">
+                              <img src = "https://cdn4.iconfinder.com/data/icons/office-34/256/10-512.png"/>
+                              <div>View Query Letter</div>
+                            </Link>
+                          </Col>
+                        </Grid>
+                      </Row> 
+                 
+                  </Row>    
+                </Grid>   
             </div>
         )
     }
 }
+
+
+
 function mapStateToProps(state){
   return{
     queryLetterResponse: state.queryLetterReducer,
