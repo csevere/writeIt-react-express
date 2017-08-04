192b2897a298d28d94d6c57e75bf8f45fe24983f
Carlita <carla.severe@gmail.com>
2017-08-04 09:37:38 -0400

    more style stuff

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-var router = express.Router();
 var mysql = require('mysql');
 var config = require('../config/config');
 var multer = require('multer');
